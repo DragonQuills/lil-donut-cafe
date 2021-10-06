9b96ac4310230c61328afbdfe18f1db8167e081c
Kayden Adams <kayden.drake.adams@gmail.com>
2021-10-06 04:37:51 -0600

    Adds setters for AnimatedSprites

--- scenes/Donut/Donut.cs
+++ scenes/Donut/Donut.cs
@@ -6,12 +6,40 @@ public class Donut : KinematicBody2D {
 	private bool dragging = false;
 	private int LEFT_MOUSE_BUTTON = 1;
 
+	private AnimatedSprite _base;
+	private AnimatedSprite _glaze;
+	private AnimatedSprite _stripes;
+	private AnimatedSprite _sprinkles;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
-		return;
+		_base = GetNode<AnimatedSprite>("Base");
+		_glaze = GetNode<AnimatedSprite>("Glaze");
+		_stripes = GetNode<AnimatedSprite>("Stripes");
+		_sprinkles = GetNode<AnimatedSprite>("Sprinkles");
+        this.setBase("vanilla");
+        this.setStripes("white");
+        this.setSprinkles("confetti");
+	} 
+	public void setBase(string baseType){
+		_base.Animation = baseType;
+        _base.Visible = true;
 	}
 
+	public void setGlaze(string glazeType){
+		_glaze.Animation = glazeType;
+        _glaze.Visible = true;
+	}
+
+	public void setStripes(string stripesType){
+		_stripes.Animation = stripesType;
+        _stripes.Visible = true;
+	}
+    
+	public void setSprinkles(string sprinklesType){
+		_sprinkles.Animation = sprinklesType;
+        _sprinkles.Visible = true;
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta) {
@@ -27,5 +55,10 @@ public class Donut : KinematicBody2D {
 				dragging = !dragging;
 			}
 		}
+		if(@event is InputEventScreenDrag eventScreenDrag){
+			// if(eventScreenTouch.Pressed && eventScreenTouch.Index == 0)
+			this.Position = eventScreenDrag.Position;
+		}
+
 	}
 }
