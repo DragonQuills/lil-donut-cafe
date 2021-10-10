b8de678b77807d31689526f9abefe4e153070adf
Kayden Adams <kayden.drake.adams@gmail.com>
2021-10-10 10:03:36 -0600

    Adds a basic "frier" with a print

---
+++ scenes/Frier/Frier.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public class Frier : Area2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+    private void _on_Frier_body_entered(Node body){
+        GD.Print("Frier entered");
+    }
+}
