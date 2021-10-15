196d8cf909ae4d9bf81dd1ba961dec5d90339ebc
Kayden Adams <kayden.drake.adams@gmail.com>
2021-10-15 00:39:48 -0600

    WIP genrealizing station code

---
+++ scenes/Stations/Station.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+[Tool]
+public class Station : Area2D
+{
+    List<Node> bodiesInStation = new List<Node>();
+    public int maxBodiesAtOnce = 1;
+
+    [Signal]
+    public delegate void DonutInStation(KinematicBody2D donut);
+    private void _on_Station_body_entered(Node body){
+        bodiesInStation.Add(body);
+    }
+    private void _on_Station_body_exited(Node body){
+        bodiesInStation.Remove(body);
+    }
+    override public void _Ready(){
+        this.Connect("body_entered", this, "_on_Station_body_entered");
+        this.Connect("body_entered", this, "_on_Station_body_exited");
+    }
+
+    private void _on_DonutReleased(Donut donut){
+        if (bodiesInStation.Contains(donut) && bodiesInStation.Count <= maxBodiesAtOnce ){
+            donut.Position = this.Position;
+            EmitSignal(nameof(DonutInStation), donut);
+        }
+    }
+}
