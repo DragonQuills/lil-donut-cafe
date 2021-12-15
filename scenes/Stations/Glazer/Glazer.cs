75000eb3bd3fddb42fd06b0a1701bce664dd3a60
Kayden Adams <kayden.drake.adams@gmail.com>
2021-12-14 17:08:50 -0700

    Add prev and next buttons to change glazes

--- scenes/Stations/Glazer/Glazer.cs
+++ scenes/Stations/Glazer/Glazer.cs
@@ -4,18 +4,44 @@ using System.Collections.Generic;
 
 public class Glazer : Station
 {
+    // TODO: Put this in a user config file
+    private string[] _glazeOptions = { "brown", "green", "pink", "tan", "white", "yellow" };
+    private int selectedGlaze = 0;
+
     override protected void _DonutInStation(Donut donut)
     {
         donut.Position = this.Position;
     }
 
-    private void _on_Button_pressed()
+    private void _on_GlazeButton_pressed()
     {
         foreach(Node body in this.bodiesInStation){
             if (body is Donut){
                 Donut donut = body as Donut;
-                donut.SetGlaze("pink");
+                donut.SetGlaze(this._glazeOptions[this.selectedGlaze]);
             }
+        }
+    }
+
+    private void _on_NextButton_pressed()
+    {
+        if(this.selectedGlaze >= this._glazeOptions.Length - 1){
+            this.selectedGlaze = 0;
+        }
+        else{
+            this.selectedGlaze += 1;
+        }
+    }
+
+    private void _on_PrevButton_pressed()
+    {
+        if (this.selectedGlaze <= 0)
+        {
+            this.selectedGlaze = this._glazeOptions.Length - 1;
+        }
+        else
+        {
+            this.selectedGlaze -= 1;
         }
     }
 }
