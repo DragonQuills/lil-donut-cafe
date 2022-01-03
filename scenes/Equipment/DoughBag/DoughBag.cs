86ed9167edfbc48c0af6e10c7cf2e50a6c952ad3
Kayden Adams <kayden.drake.adams@gmail.com>
2022-01-03 10:02:29 -0700

    Changes DoughBag to be a station

--- scenes/Equipment/DoughBag/DoughBag.cs
+++ scenes/Equipment/DoughBag/DoughBag.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class DoughBag : Area2D
+public class DoughBag : Station
 {
     public void _on_DoughBag_input_event(object viewport, object @event, int shape_idx)
     {
